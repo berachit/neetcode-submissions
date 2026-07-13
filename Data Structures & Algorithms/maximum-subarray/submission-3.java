33d8d8d29a4beff9a352f9a361ee66234b67c854
Rachit Srivastava <rachitsrivastava0304@gmail.com>
2026-07-14 03:21:08 +0530

    Add: maximum-subarray - submission-3

---
+++ Data Structures & Algorithms/maximum-subarray/submission-3.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        // Kadanes Algo
+        int n = nums.length;
+        int sum = 0;
+        int ans = Integer.MIN_VALUE;
+
+        for(int i = 0; i < n ; i++){
+            sum += nums[i];
+            ans = Math.max(ans, sum);
+            if(sum < 0){
+                sum = 0;
+            }
+        }
+        return ans;
+    }
+}
